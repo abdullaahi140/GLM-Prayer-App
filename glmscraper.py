ef34f567ae67df1a13d5b434eb5ea84ec2fd6581
Abdullaahi Farah <abdullaahi140@gmail.com>
2017-11-26 20:45:48 +0000

    0.0.2 - Database up and running

--- glmscraper.py
+++ glmscraper.py
@@ -1,30 +1,44 @@
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs 
 import lxml
+import sqlite3
 
-def main():
-    r = requests.get("http://www.greenlanemasjid.org/prayer-times/november-2017/")
+class Timetable():
 
-    soup = BeautifulSoup(str(r.text), "lxml")
-    dirtydata = soup.find_all('ol')
-    dirtydata = spring_clean(dirtydata)
+    def main():
+        r = requests.get("http://www.greenlanemasjid.org/prayer-times/november-2017/")
+        soup = bs(str(r.text), "lxml")
+        dirtydata = soup.find_all('ol')
+        dirtydata = Timetable.spring_clean(dirtydata)
 
-    soup = BeautifulSoup(dirtydata, "lxml")
-    dateandtimes = []
-    for i in soup.select("[class~=p-prayer-table-row__cell]"):
-        dateandtimes.append(i.string)
+        soup = bs(dirtydata, "lxml")
+        dateandtimes = []
+        for i in soup.select("[class~=p-prayer-table-row__cell]"):
+            dateandtimes.append(i.string)
 
-    daylist = []
-    k=0
-    for i in range(int((len(dateandtimes)/12))):
-        x= (dateandtimes[k:(k+12)])
-        daylist.append(x)
-        k+=12
-    
-    print(daylist)
+        daylist = []
+        k=0
+        for i in range(int((len(dateandtimes)/12))):
+            daylist.append(dateandtimes[k:(k+12)])
+            k+=12
 
-def spring_clean(offender):
-    offender = str(offender)[1:-1]
-    return offender
+        Timetable.create_database(daylist)
 
-main()
+    def spring_clean(offender):
+        offender = str(offender)[1:-1]
+        return offender
+
+    def create_database(dtlist):
+        conn = sqlite3.connect("timetable.db") 
+        c = conn.cursor()
+        c.execute("CREATE TABLE IF NOT EXISTS October(Day TEXT, FajrStart TEXT, FajrJamat TEXT, Sunrise TEXT, DhuhrStart TEXT, DhuhrJamat TEXT, AsrStart TEXT, AsrJamat TEXT, Maghrib TEXT, IshaStart TEXT, IshaJamat TEXT)")
+
+        for i in dtlist:
+            c.execute("INSERT INTO October (Day, FajrStart, FajrJamat, Sunrise, DhuhrStart, DhuhrJamat, AsrStart, AsrJamat, Maghrib, IshaStart, IshaJamat) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                            (i[0], i[1], i[2], i[3], i[4], i[5], i[6], i[7], i[8], i[9], i[10]))
+        conn.commit()
+
+if __name__ == '__main__':
+    Timetable.main()
+
+print("Finished")
