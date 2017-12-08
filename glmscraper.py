89672552a7bc2d45136cda1859c5b6ed7ca91f5e
Abdullaahi Farah <abdullaahi140@gmail.com>
2017-12-08 21:59:19 +0000

    0.0.3 - GUI up and running
    Extremely buggy. Lots needs fixing.

--- glmscraper.py
+++ glmscraper.py
@@ -1,44 +1,61 @@
 import requests
-from bs4 import BeautifulSoup as bs 
+from bs4 import BeautifulSoup as bs
 import lxml
 import sqlite3
+import datetime
 
-class Timetable():
 
-    def main():
-        r = requests.get("http://www.greenlanemasjid.org/prayer-times/november-2017/")
+class Database():
+
+    def get_data(month):
+        r = requests.get("http://www.greenlanemasjid.org/prayer-times/%s-2017/" %month)
         soup = bs(str(r.text), "lxml")
         dirtydata = soup.find_all('ol')
-        dirtydata = Timetable.spring_clean(dirtydata)
+        dirtydata = Database.spring_clean(dirtydata)
 
         soup = bs(dirtydata, "lxml")
         dateandtimes = []
         for i in soup.select("[class~=p-prayer-table-row__cell]"):
             dateandtimes.append(i.string)
 
+        k = 0
         daylist = []
-        k=0
         for i in range(int((len(dateandtimes)/12))):
             daylist.append(dateandtimes[k:(k+12)])
-            k+=12
-
-        Timetable.create_database(daylist)
+            k += 12
+        Database.create_database(daylist,month=month)
 
     def spring_clean(offender):
         offender = str(offender)[1:-1]
         return offender
 
-    def create_database(dtlist):
-        conn = sqlite3.connect("timetable.db") 
+    def create_database(dtlist,month):
+        conn = sqlite3.connect("timetable.db")
         c = conn.cursor()
-        c.execute("CREATE TABLE IF NOT EXISTS October(Day TEXT, FajrStart TEXT, FajrJamat TEXT, Sunrise TEXT, DhuhrStart TEXT, DhuhrJamat TEXT, AsrStart TEXT, AsrJamat TEXT, Maghrib TEXT, IshaStart TEXT, IshaJamat TEXT)")
+        c.execute("CREATE TABLE IF NOT EXISTS %s(Day TEXT, Date TEXT, FajrStart TEXT, FajrJamat TEXT, Sunrise TEXT, DhuhrStart TEXT, DhuhrJamat TEXT, AsrStart TEXT, AsrJamat TEXT, Maghrib TEXT, IshaStart TEXT, IshaJamat TEXT)" %month)
 
         for i in dtlist:
-            c.execute("INSERT INTO October (Day, FajrStart, FajrJamat, Sunrise, DhuhrStart, DhuhrJamat, AsrStart, AsrJamat, Maghrib, IshaStart, IshaJamat) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-                            (i[0], i[1], i[2], i[3], i[4], i[5], i[6], i[7], i[8], i[9], i[10]))
+            c.execute("INSERT INTO %s (Day, Date, FajrStart, FajrJamat, Sunrise, DhuhrStart, DhuhrJamat, AsrStart, AsrJamat, Maghrib, IshaStart, IshaJamat) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)" %month,
+                            (i[0], (i[1][:-2]), i[2], i[3], i[4], i[5], i[6], i[7], i[8], i[9], i[10], i[11]))
         conn.commit()
+        conn.close()
+
+
+class todaysDate():
+
+    def today():
+        date = datetime.date.today().strftime("%#d")
+        month = datetime.date.today().strftime("%B")
+        return todaysDate.currentdaydata(date,month)
+
+    def currentdaydata(date,month):
+        conn = sqlite3.connect("timetable.db")
+        c = conn.cursor()
+        c.execute("SELECT * FROM %s WHERE Date = ?" %month, (date))
+        return c.fetchone()
+
 
 if __name__ == '__main__':
-    Timetable.main()
-
-print("Finished")
+    Database.get_data("November")
+    print(todaysDate.today())
+    print("Finished")
--- temp.py
+++ temp.py
@@ -2,13 +2,16 @@
 import sys
 app = Qt.QApplication(sys.argv)
 systemtray_icon = Qt.QSystemTrayIcon(app)
-systemtray_icon.setIcon(Qt.QIcon("vscode-good.ico")) 
+systemtray_icon.setIcon(Qt.QIcon("temperature.ico")) 
 systemtray_icon.show()
 systemtray_icon.showMessage("Biriyani", "Gotta love some biriyani")
+
 """
+
 from win10toast import ToastNotifier
 toaster = ToastNotifier()
 toaster.show_toast("Biriyani",
               "I want some now",
-              icon_path="vscode-good.ico",
+              icon_path="\\temperature.ico",
               duration=10)
+
