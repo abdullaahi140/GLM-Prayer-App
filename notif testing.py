a144853b4af6532d89b432833f8e4272ca5517f7
Abdullaahi Farah <abdullaahi140@gmail.com>
2018-01-23 21:43:20 +0000

    Testing complete
    Integrated into notifications.pyw and App.pyw

--- notif testing.py
+++ notif testing.py
@@ -1,17 +1,19 @@
-"""from PyQt5 import Qt
+from PyQt5 import Qt
 import sys
 app = Qt.QApplication(sys.argv)
+icon = Qt.QIcon("C:\\Users\\abdul\\Documents\\Python Scripts\\Green Lane Timetable Scraper git\\icons\\islamic3.png")
 systemtray_icon = Qt.QSystemTrayIcon(app)
-systemtray_icon.setIcon(Qt.QIcon("temperature.ico")) 
+systemtray_icon.setIcon(icon) 
+systemtray_icon.setToolTip("GLM Prayer Timetable")
 systemtray_icon.show()
-systemtray_icon.showMessage("Biriyani", "Gotta love some biriyani")
+systemtray_icon.showMessage("Asr Start in 15 minutes", "You're welcome", icon)
+print(systemtray_icon.isVisible())
+
+while True:
+    print("\o/")
 
 """
-
 from win10toast import ToastNotifier
 toaster = ToastNotifier()
-toaster.show_toast("Biriyani",
-              "I want some now",
-              icon_path="\\temperature.ico",
-              duration=10)
-
+toaster.show_toast()
+"""
--- running processes.py
+++ running processes.py
@@ -4,7 +4,7 @@ import psutil
 running = []
 for proc in psutil.process_iter():
     try:
-        if "temp.py" in proc.cmdline()[1]:
+        if "notifications.pyw" in proc.cmdline()[1]:
             running.append([proc.name(), proc.pid, proc.cmdline()[1]])
     except (psutil._exceptions.AccessDenied, IndexError):
         pass
