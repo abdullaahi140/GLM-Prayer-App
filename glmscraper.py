b7ff134c53c707ca428e8559a5dd5ec598d906b6
Abdullaahi Farah <abdullaahi140@gmail.com>
2018-02-26 11:08:50 +0000

    0.0.6 - Added function to return datetime objects of list
    Got rid of today(). Added functions to parse strings or datetime objects. Added function to return prayer times in 12 or 24 hour formats

--- glmscraper.py
+++ glmscraper.py
@@ -9,20 +9,20 @@ class Database():
     def get_data(month):
         r = requests.get("http://www.greenlanemasjid.org/prayer-times/%s/" %(month[:-4]+"-"+month[-4:]))
         tree = html.fromstring(r.text)
-        dateandtimes = [i.text for i in tree.cssselect("li.p-prayer-table-row__cell")]
+        prayertimes = [i.text for i in tree.cssselect("li.p-prayer-table-row__cell")]
 
         k = 0
-        daylist = []
-        for i in range(int((len(dateandtimes)/12))):
-            daylist.append(dateandtimes[k:(k+12)])
+        ptlist = []
+        for i in range(int((len(prayertimes)/12))):
+            ptlist.append(prayertimes[k:(k+12)])
             k += 12
-        Database.create_database(daylist,month=month)
+        Database.create_database(ptlist,month=month)
 
-    def create_database(dtlist,month):
+    def create_database(ptlist,month):
         conn = sqlite3.connect("timetable.db")
         c = conn.cursor()
         c.execute("CREATE TABLE IF NOT EXISTS %s (Day TEXT, Date TEXT, FajrStart TEXT, FajrJamat TEXT, Sunrise TEXT, DhuhrStart TEXT, DhuhrJamat TEXT, AsrStart TEXT, AsrJamat TEXT, Maghrib TEXT, IshaStart TEXT, IshaJamat TEXT)" %month)
-        for i in dtlist:
+        for i in ptlist:
             c.execute("INSERT INTO %s (Day, Date, FajrStart, FajrJamat, Sunrise, DhuhrStart, DhuhrJamat, AsrStart, AsrJamat, Maghrib, IshaStart, IshaJamat) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)" %month, (i[0],i[1],i[2],i[3],i[4],i[5],i[6],i[7],i[8],i[9],i[10],i[11]))
             conn.commit()
         conn.close()
@@ -30,26 +30,43 @@ class Database():
 
 class dateData():
 
-    def today():
-        date = datetime.date.today().strftime("%#d")
-        month = datetime.date.today().strftime("%B%Y")
-        return dateData.currentdaydata(date,month)
+    def dt_grab_prayer_times(date,dateincrement=0,twelve=True,twofour=None):
+        date = date + datetime.timedelta(days=dateincrement)
+        day = date.strftime("%#d")
+        month = date.strftime("%B%Y")
+        tt = dateData.grab_prayer_times(day,month,twelve=twelve,twofour=twofour)
+        return tt, date, day, month
 
-    def currentdaydata(date,month):
+    def grab_prayer_times(day,month,twelve=None,twofour=None):
         conn = sqlite3.connect("timetable.db")
         c = conn.cursor()
         try:
-            c.execute("SELECT * FROM %s WHERE ROWID = ?" %month, [date])
+            c.execute("SELECT * FROM %s WHERE ROWID = ?" %month, [day])
         except sqlite3.OperationalError:
             Database.get_data(month)
-            c.execute("SELECT * FROM %s WHERE ROWID = ?" %month, [date])
-        tt_list = c.fetchone()
+            c.execute("SELECT * FROM %s WHERE ROWID = ?" %month, [day])
+        try:
+            tt_list = dateData.am_or_pm(list(c.fetchone()),twelve=twelve,twofour=twofour)
+        except TypeError:
+            tt_list = None
         conn.close()
         return tt_list
 
+    def dt_tt(tt,day="",month="",twelve=True,twofour=None,hourformat="%I:%M%p",yearformat=""):  
+        tt = tt[:2] + [datetime.datetime.strptime(i+day+month, hourformat+yearformat) for i in tt[2:]]
+        return tt
+
+    def am_or_pm(tt,twelve=None,twofour=None,hourformat="%I:%M%p"):
+        if twelve is True:
+            strfformat = "%I:%M %p"
+        if twofour is True:
+            strfformat = "%H:%M"
+
+        tt = dateData.dt_tt(tt,twelve=twelve,twofour=twofour,hourformat=hourformat)
+        tt = tt[:2] + [datetime.datetime.strftime(i, strfformat) for i in tt[2:]]
+        return tt
+
 
 if __name__ == '__main__':
-
-    Database.get_data("January2018")
-    print(dateData.today())
-    print("Finished")
+    print (dateData.dt_grab_prayer_times(datetime.datetime.today(),0,True,None)[0])
+    # print(dateData.grab_prayer_times("26","November2017",twofour=True))
